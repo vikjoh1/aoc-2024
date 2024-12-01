d792ccd540b13b5c39e6bb17cd6ee0325de67112
vikjoh1 <vikjoh11@gmail.com>
2024-12-01 13:51:03 +0100

    Day1 part1

--- day1/part1.go
+++ day1/part1.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	fmt.Print("Part1")
+	file, err := os.Open("day1/input.txt")
+	if err != nil {
+		fmt.Println("error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	var total int
+
+	var leftIDs, rightIDs []int
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		cols := strings.Fields(line)
+		if len(cols) == 2 {
+			left, _ := strconv.Atoi(cols[0])
+			right, _ := strconv.Atoi(cols[1])
+			leftIDs = append(leftIDs, left)
+			rightIDs = append(rightIDs, right)
+		}
+	}
+
+	sort.Ints(leftIDs)
+	sort.Ints(rightIDs)
+
+	for idx := range leftIDs {
+		if leftIDs[idx] > rightIDs[idx] {
+			total += leftIDs[idx] - rightIDs[idx]
+		} else {
+			total += rightIDs[idx] - leftIDs[idx]
+		}
+	}
+
+	fmt.Println(total)
 }
