222fc3b25315bf806f909c7ce14f9681b61f44e4
vikjoh1 <vikjoh11@gmail.com>
2024-12-05 15:41:14 +0100

    Day3 part1

---
+++ day3/part1.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+)
+
+func main() {
+	file, err := os.Open("day3/input.txt")
+	if err != nil {
+		fmt.Println("error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	var allResults [][3]int
+	sum := 0
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		text := scanner.Text()
+		re := regexp.MustCompile(`mul\((\d+),\s*(\d+)\)`)
+		matches := re.FindAllStringSubmatch(text, -1)
+
+		for _, match := range matches {
+			num1, _ := strconv.Atoi(match[1])
+			num2, _ := strconv.Atoi(match[2])
+			result := num1 * num2
+			allResults = append(allResults, [3]int{num1, num2, num1 * num2})
+			sum += result
+		}
+	}
+
+	fmt.Println(sum)
+}
